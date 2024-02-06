cd1986647d7b7b146ca317570c434d363837ae0b
Manasa <manasare94@gmail.com>
2024-02-07 00:04:31 +0530

    Created Footer Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
    <>
    <Header/>
+   <Container>
+    <main>
+      
+    </main>
+   </Container>
+   <Footer/>
    </>
   );
 }
